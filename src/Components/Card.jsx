fc451503562f3a7dd31187b428d0c4d7d056b5a7
lucaperullo <lucaperullo@outlook.it>
2021-10-12 15:25:15 +0200

    up

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -4,7 +4,7 @@ export default function Card(props) {
   return (
     <div className="card-container">
       <div className="card-tag">{props.tags[0]}</div>
-      <img src={props.image} alt="" className="card-image" />
+      <img src={props.image} alt={props.tilte} className="card-image" />
       <div className="card-text">
         <h3 className="card-title">{props.title}</h3>
         <span className="card-text-muted">{props.description}</span>
