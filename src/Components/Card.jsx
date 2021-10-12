3d677d701a725271939d2f013f2ce68ec118f988
lucaperullo <lucaperullo@outlook.it>
2021-10-12 22:16:19 +0200

    up

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -4,6 +4,7 @@ import { FiHeart } from "react-icons/fi";
 import { FaHeart } from "react-icons/fa";
 import { FaHeartBroken } from "react-icons/fa";
 export default function Card(props) {
+  const [like, setLike] = React.useState(false);
   return (
     <motion.div layout className="card-container">
       <div className="card-tag">{props.tags[0]}</div>
@@ -23,17 +24,23 @@ export default function Card(props) {
           <span className="card-text-muted">{props.description}</span>
         </div>
         <div className="like-button">
-          {props.liked ? (
+          {props.liked || like ? (
             <div className="liked">
               <FaHeart className="like" />
               <FaHeartBroken
-                onClick={() => localStorage.removeItem(`${props.id}`)}
+                onClick={() => {
+                  localStorage.removeItem(`${props.id}`);
+                  setLike(false);
+                }}
                 className="unlike"
               />
             </div>
           ) : (
             <FiHeart
-              onClick={() => localStorage.setItem(`${props.id}`, props.id)}
+              onClick={() => {
+                localStorage.setItem(`${props.id}`, props.id);
+                setLike(true);
+              }}
               className="not-liked"
             />
           )}
